a4b6ad708dad17e25c7bc70fb393ce8a30bbab44
rhgha <elim9010@gmail.com>
2022-03-10 21:43:31 +0900

    1

--- process.c
+++ process.c
@@ -1,7 +1,7 @@
 #include "cereal.h"
 
 #define PRO_FILE_NAME "process"
-#define MAT_FILE_NAME "material"
+#define MAT_FILE_NAME "test_pro_material"
 
 #define PLAN_NUM 100
 #define PLAN_CODE "A1001"
@@ -17,14 +17,16 @@ typedef struct _req_code {
 	struct _req_code* next;
 }req_code;
 
-//void init(void);
+void init(void);
 void bg_process(void);
-//req_code* NewNode();
 
 void check_parts(int num, char* bom_res, req_code*);			// 자체생산부품 필요량 파악
 int parts_produce(int num, char* bom_res, char* code);		// 부족한 부품 생성/ 자재에 자품목 수량 업로드
-give_LOT();		// 생산품 LOT번호 생성
-produce_product();		//생산계획 품목 자재에 생산 수량 업로드
+//give_LOT();		// 생산품 LOT번호 생성
+//produce_product();		//생산계획 품목 자재에 생산 수량 업로드
+void pro_material_use(char* p_code);
+void pro_material_create(char* p_code);
+
 req_code* New(req_code* head, int num, char* code);
 
 void process(void)
@@ -37,39 +39,13 @@ void process(void)
 
 	bg_process();
 
-	system("cls");
-
-	if (input < 0 || input >2)
-	{
-		printf("오류입니다. 다시 입력하세요.\n");
-		getchar();
-		getchar();
-		system("cls");
-
-		process();
-	}
-
-	switch (input)
-	{
-	case 1:
-		printf("공정을 등록합니다.\n");
-		printf("");
-
-		break;
-	case 2:
-		printf("공정을 조회합니다.\n");
-		//view();
-		break;
-	case 0:
-		break;
-	}
 }
 
 void bg_process(void)
 {
-	char* bom_res;
-	char* con = "A1000S";
-	BOM_SEARCH(con);
+	char* con = "*";
+	BOM_TREE* bom_res = BOM_SEARCH(con);
+	BOM_Forward_PrintTree(bom_res, bom_res->NODE_CODE);
 
 	req_code* head = (req_code*)malloc(sizeof(req_code));
 
@@ -80,7 +56,18 @@ void bg_process(void)
 	//produce_product();		//생산계획 품목 생산 및 등록
 	//material_upload();		//생산자제 등록
 }
+void init(void)
+{
+	_create(MAT_FILE_NAME, "PRD_CODE VARCHAR(6) STATUS VARCHAR(6) DATE INT ACC_CODE VARCHAR(6)");
+	initalizing(MAT_FILE_NAME);
+	char* val[30] =
+	{ "",
 
+	};
+	for(int i =0;i<30;i++)	_insert(val[i]);
+
+	file_column_free();
+}
 void check_parts(int num, char* bom_res, req_code* head)
 {
 	//_BOM_Backward_PrintTree();
@@ -107,7 +94,7 @@ int produce_parts(req_code* head)
 		p_code = cur->code;
 		while (cnt < p_num)
 		{
-
+			pro_material_use(p_code);
 			cnt++;
 		}
 		cur = cur->next;
@@ -122,14 +109,52 @@ req_code* New(req_code* head, int num, char* code)
 	NewNode->next = head->next;
 	head->next = NewNode;
 }
-//void init(void)
-//{
-//	_create("FILE_NAME","'품목코드' INT '자재코드' INT '수량' INT");
-//	initalizing("FILE_NAME");
-//	_insert("1001,2001,100");
-//	_insert("1002,2002,100");
-//	_insert("1003,2003,100");
-//	_insert("1004,2004,100");
-//	_insert("1005,2005,100");
-//
-//}
+
+void pro_material_create(char* p_code) {
+
+	if (initalizing(MAT_FILE_NAME) == -1) {
+		printf("%s\n", err_msg);
+
+		file_column_free();
+		return -1;
+	}
+
+	/*if (_insert() == -1) {
+		printf("%s\n", err_msg);
+
+		file_column_free();
+		return -1;
+	}*/
+
+	print_data();
+	printf("\n");
+	file_column_free();
+
+	system("pause");
+	system("cls");
+	stock();
+}
+void pro_material_use(char* p_code) {
+
+	if (initalizing(MAT_FILE_NAME) == -1) {
+		printf("%s\n", err_msg);
+
+		file_column_free();
+		return -1;
+	}
+
+	if (_select() == -1) {
+		printf("%s\n", err_msg);
+
+		file_column_free();
+		return -1;
+	}
+
+	print_data();
+	printf("\n");
+	file_column_free();
+
+	system("pause");
+	system("cls");
+	stock();
+}
